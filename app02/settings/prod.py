358b7120495bfbf7ed0f81d2a67b00fc93377cd5
Osvaldo Bustamante <rasuz.counsulta@gmail.com>
2023-05-09 22:20:00 -0400

    segunda subida

---
+++ app02/settings/prod.py
@@ -0,0 +1,35 @@
+import os
+from .base import *
+
+from decouple import config
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': config('ENGINE'),
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT'),
+    }
+}
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+WKHTMLTOPDF_BIN_PATH = '/usr/local/bin/wkhtmltopdf'
+
+PDFKIT_CONFIG = {
+    'wkhtmltopdf': '/usr/local/bin/wkhtmltopdf'
+}
